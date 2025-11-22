d210b74d77d2fc8046435c32c2565f679ef150bf
ffuze <alekeggsdee92@gmail.com>
2025-11-22 14:25:00 +0100

    Started CPP00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+static void mega_capitalizer(int argc, char *argv[])
+{
+	std::string sentence;
+	
+	sentence = "\0";
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (islower(argv[i][j]))
+				sentence += toupper(argv[i][j]);
+			else if (argv[i][j] != '"')
+				sentence += argv[i][j];
+			else if (argv[i][j] == ' ')
+				sentence += " ";
+		}
+	}
+	std::cout << sentence << std::endl;
+}
+
+static void very_loud_noise(void)
+{
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	if (argc < 1)
+		return (0);
+	else if (argc < 2)
+		very_loud_noise();
+	else
+		mega_capitalizer(argc, argv);
+	return (0);
+}
---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,8 @@
+#include "Contacts.hpp"
+
+class PhoneBook : public Contacts
+{
+    public:
+        Contacts contacts[8];
+        PhoneBook(std::string name, std::string surname, std::string phone, Contacts contacts[])
+}
