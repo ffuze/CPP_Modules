5d36a582720bb1445d06e527020545fe3b21e29a
Alex <alekeggsdee92@gmail.com>
2026-01-17 13:29:07 +0100

    added reference to stringREF

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -21,7 +21,7 @@ int main(void)
 
     std::cout << "Memory address of string: " << &string << std::endl;
     std::cout << "Memory address in stringPTR: " << stringPTR << std::endl;
-    std::cout << "Memory address in stringREF: " << stringREF << std::endl << std::endl;
+    std::cout << "Memory address in stringREF: " << &stringREF << std::endl << std::endl;
 
     std::cout << "Value of string: " << string << std::endl;
     std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
