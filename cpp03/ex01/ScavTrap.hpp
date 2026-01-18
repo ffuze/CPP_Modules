185d26ae61992719fdd5d3210c1318093cc48d45
ffuze <alekeggsdee92@gmail.com>
2026-01-18 19:46:40 +0100

    deleted duplicate destructor

--- cpp03/ex01/ScavTrap.hpp
+++ cpp03/ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: adegl-in <adegl-in@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 12:19:35 by adegl-in          #+#    #+#             */
-/*   Updated: 2026/01/18 19:22:16 by adegl-in         ###   ########.fr       */
+/*   Updated: 2026/01/18 19:45:02 by adegl-in         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ class ScavTrap : public ClapTrap
         ~ScavTrap();
         ScavTrap(const ScavTrap& other);
         ScavTrap& operator=(const ScavTrap& other);
-        ~ScavTrap();
         void attack(const std::string& target);
         void guardGate();
 };
