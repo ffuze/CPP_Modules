cdddfb5aa9d126170837fc003c79f074bd1b2d54
ffuze <alekeggsdee92@gmail.com>
2026-02-18 17:36:33 +0100

    added main provided by the subject

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -1,20 +1,17 @@
 #include "templates.hpp"
 
-int main()
-{
-    std::cout << "Max of 5 and 5 is: " << myMax<int>(5, 5) << std::endl;
-    std::cout << "Max of 3.5 and 7.5 is: " << myMax<double>(3.5, 7.5) << std::endl;
-    std::cout << "Max of 'g' and 'e' is: " << myMax<char>('g', 'e') << std::endl;
-    
-    std::cout << "Min of 5 and 5 is: " << myMin<int>(5, 5) << std::endl;
-    std::cout << "Min of 3.5 and 7.5 is: " << myMin<double>(3.5, 7.5) << std::endl;
-    std::cout << "Min of 'g' and 'e' is: " << myMin<char>('g', 'e') << std::endl;
-
-    int a = 6;
-    int b = 7;
-    std::cout << "Before: a = " << a << ", b = " << b << std::endl;
-    mySwap(a, b);
-    std::cout << "Now: a = " << a << ", b = " << b << std::endl;
-
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    mySwap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::myMin( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::myMax( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    mySwap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::myMin( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::myMax( c, d ) << std::endl;
     return 0;
 }
