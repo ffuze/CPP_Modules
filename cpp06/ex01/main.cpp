4123db8210fb25212b4985ee5f7789df417ecdcc
ffuze <alekeggsdee92@gmail.com>
2026-02-22 16:34:48 +0100

    fixed issue which led the program not to return the same pointer to the data structure

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -3,25 +3,24 @@
 int main()
 {
     playerData.nickname = "alek#R1M";
-    playerData.hoursPlayed = 1000;
+    playerData.hoursPlayed = 1500;
     playerData.rank = 'P';
 
-    std::cout << "Original Data:\n" << playerData << std::endl;
-    std::cout << "Address: " << &playerData << std::endl;
-    std::cout << std::endl;
+    std::cout << "Original data:\n" << playerData;
+    std::cout << "Struct address: " << &playerData << std::endl;
 
     uintptr_t serialized = Serialization::serialize(&playerData);
-    std::cout << "Serialized to uintptr_t: " << serialized << std::endl;
+    std::cout << "\nSerialized address: " << serialized << std::endl;
     std::cout << std::endl;
 
     Data* deserialized = Serialization::deserialize(serialized);
-    std::cout << "Deserialized Data:\n" << *deserialized << std::endl;
-    std::cout << "Address: " << deserialized << std::endl;
+    std::cout << "Deserialized data:\n" << *deserialized;
+    std::cout << "Address got from serialized data: " << deserialized << std::endl;
     std::cout << std::endl;
     if (&playerData == deserialized)
-        std::cout << "Pointers are matching" << std::endl;
+        std::cout << "Pointers (addresses) are the same !" << std::endl;
     else
-        std::cout << "Pointers are NOT matching" << std::endl;
+        std::cout << "Pointers (addresses) are not matching (wtf how)" << std::endl;
 
     return 0;
 }
