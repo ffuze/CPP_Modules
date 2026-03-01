d935b86bb6b87f3ea675ee94d3376dc982b9a820
ffuze <alekeggsdee92@gmail.com>
2026-03-01 17:21:59 +0100

    finished ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,7 +1,7 @@
 #ifndef EASYFIND_HPP
-#define EASYFIND_HPP
+# define EASYFIND_HPP
 
-#include <bits/stdc++.h>
+# include <bits/stdc++.h>
 
 class OccurrenceNotFoundException : public std::exception
 {
@@ -18,21 +18,19 @@ void easyfind(T &cont, int occurrence)
 {
 	try
 	{
-		// for (typename T::iterator it = cont.begin(); it != cont.end(); it++)
-		// {
+		// for (typename T::iterator it = cont.begin(); it != cont.end(); it++) {}
 		typename T::iterator it = std::find(cont.begin(), cont.end(), occurrence);
 		if (it != cont.end())
 		{
 			std::cout << "Found it: " << *it << std::endl;
 			return;
 		}
-			// }
-		throw(OccurrenceNotFoundException());
-		}
-		catch (std::exception &e)
-		{
-			std::cout << e.what() << std::endl;
-		}
+		throw (OccurrenceNotFoundException());
 	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -2,11 +2,34 @@
 
 int main()
 {
-	std::list<int> cont(67);
-	for (size_t i = 0; i < 70; i++)
+	const unsigned int contSize = 67;
+	std::list<int> cont(contSize);
+	for (size_t i = 0; i < 68; i++)
 	{
-		std::cout << i << std::endl;
 		cont.push_back(i);
-	}	
-	easyfind(cont, 67);
+	}
+	std::cout << std::endl;
+	std::cout << "easyfind(MAX): ";
+	easyfind(cont, contSize);
+	std::cout << std::endl;
+	std::cout << "easyfind(0): ";
+	easyfind(cont, 0);
+	std::cout << std::endl;
+	std::cout << "easyfind(OOB): ";
+	easyfind(cont, 69);
+	std::cout << std::endl;
+	std::cout << "easyfind(negative): ";
+	easyfind(cont, -1);
+	std::cout << std::endl;
+	std::cout << "easyfind(INTMAX): ";
+	easyfind(cont, 2147483647);
+	std::cout << std::endl;
+	std::cout << "easyfind(INTMIN): ";
+	easyfind(cont, -2147483647);
+	std::cout << std::endl;
+	std::cout << "easyfind(alpha): ";
+	easyfind(cont, 'a');
+	std::cout << std::endl;
+	std::cout << "easyfind(consecutive): ";
+	easyfind(cont, 6768);
 }
